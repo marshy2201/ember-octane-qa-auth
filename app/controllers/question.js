23be56dc344c2c55d910969b0b396d3b76509214
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-06-22 16:54:00 +0100

    sort answers by vote and highlight highest voted answer

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -1,9 +1,22 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { sort } from '@ember/object/computed';
 
 export default class QuestionController extends Controller {
   @tracked answer = null;
+
+  /**
+   * Sort answers with highest votes
+   */
+  @sort('model.answers', function(a, b) {
+    if (b.votes > a.votes) {
+      return 1;
+    } 
+    
+    return -1;
+  }) 
+  answers;
 
   /*
    * Add Answer

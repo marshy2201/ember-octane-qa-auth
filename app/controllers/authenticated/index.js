703bc1206b3bb39d29ddb7f2c3cc074fe4e021c9
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-06-30 17:55:15 +0100

    display question createdBy field, question deleted only by the createdBy user

--- app/controllers/authenticated/index.js
+++ app/controllers/authenticated/index.js
@@ -2,8 +2,11 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { sort } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default class IndexController extends Controller {
+  @service currentUser;
+  
   @tracked question = null;
 
   /**

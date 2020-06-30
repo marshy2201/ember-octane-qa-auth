35b3685a04023c9c02c1591b2c5fcf3faa9e52a9
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-06-30 17:54:00 +0100

    createdBy field added to question model

--- app/models/question.js
+++ app/models/question.js
@@ -1,4 +1,4 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class QuestionModel extends Model {
   @attr('string') text;
@@ -8,4 +8,6 @@ export default class QuestionModel extends Model {
     }
   }) createdAt;
   @attr answers;
+
+  @belongsTo('user') createdBy;
 }

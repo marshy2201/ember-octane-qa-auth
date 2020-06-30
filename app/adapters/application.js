29e3b25991fe48558c7eeb2e7485019bf4af5b9e
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-06-30 13:40:27 +0100

    auth header added to requests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,20 @@
 import RESTAdapter from '@ember-data/adapter/rest';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default class ApplicationAdapter extends RESTAdapter {
+  @service session;
+
   host = "http://localhost:3000";
+
+  @computed('session.data.authenticated.access_token')
+  get headers() {
+    let headers = {};
+    
+    if (this.session.isAuthenticated) {
+      headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+
+    return headers;
+  }
 }
